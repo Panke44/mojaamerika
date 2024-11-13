63a4ecab3c1436653cbe2d105c6a64ba1c2b6f79
Milos Panic <milos@Miloss-MacBook-Pro.local>
2024-11-13 18:16:01 +0100

    navbar mobile logo switch

--- src/Components/General/MobileNavbar.jsx
+++ src/Components/General/MobileNavbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import logo from '../../Images/logo.png'
+// import logo from '../../Images/logo.png'
 import logo2 from '../../Images/favicon.png'
 import OrangeBtn from './OrangeBtn'
 
